5bfc59b3924a44683480d540e958ae8969c9b88b
Jannik Möbius <moebius@kv-gmbh.de>
2023-02-22 14:04:17 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+apt update
+
+apt install zsh -y
+
+# create new .zshrc
+if [-f ~/.zshrc]; then
+  rm ~/.zshrc
+fi
+
+# download zshrc to ~/.zshrc
+
+# download kali theme to ~/.oh-my-zsh/themes/
+
+# install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# install zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+# set permissions
+chmod g-w,o-w ~./oh-my-zsh/custom/plugins/zsh-completions
+chmod g-w,o-w ~./oh-my-zsh/custom/plugins/zsh-completions/src
+
+
